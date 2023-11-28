df9a95e3f491826bc4e86376c97e126c2bfeaf22
Malte <91071750+Loxiuz@users.noreply.github.com>
2023-11-28 12:47:35 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -31,44 +31,44 @@ app.get("/sponsors", async (req, res) => {
   );
 });
 
-app.delete("/artists/:id", async (request, response) => {
-  const id = request.params.id;
-  const query = "DELETE FROM artists WHERE artistId=?;";
-  const values = [id];
+// app.delete("/artists/:id", async (request, response) => {
+//   const id = request.params.id;
+//   const query = "DELETE FROM artists WHERE artistId=?;";
+//   const values = [id];
 
-  connection.query(query, values, (error, results) => {
-    errorResult(error, results, response);
-  });
-});
-app.post("/artists", async (request, response) => {
-  const reqBody = request.body;
-  connection.query(
-    "INSERT INTO artists(name, birthdate, activeSince, labels, website, genres, shortDescription, image) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
-    [
-      reqBody.name,
-      reqBody.birthdate,
-      reqBody.activeSince,
-      reqBody.labels,
-      reqBody.website,
-      reqBody.genres,
-      reqBody.shortDescription,
-      reqBody.image,
-    ],
-    (err, result) => {
-      // print error or respond with result.
-      errorResult(err, result, response);
-    }
-  );
-});
-app.put("/artists/:id", async (request, response) => {
-  const reqArtistId = request.params.id;
-  const reqBody = request.body;
-  connection.query(
-    "UPDATE artists SET name = ?, birthdate = ? WHERE artistId = ?",
-    [reqBody.name, reqBody.birthdate, reqArtistId],
-    (err, result) => {
-      // print error or respond with result.
-      errorResult(err, result, response);
-    }
-  );
-});
+//   connection.query(query, values, (error, results) => {
+//     errorResult(error, results, response);
+//   });
+// });
+// app.post("/artists", async (request, response) => {
+//   const reqBody = request.body;
+//   connection.query(
+//     "INSERT INTO artists(name, birthdate, activeSince, labels, website, genres, shortDescription, image) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+//     [
+//       reqBody.name,
+//       reqBody.birthdate,
+//       reqBody.activeSince,
+//       reqBody.labels,
+//       reqBody.website,
+//       reqBody.genres,
+//       reqBody.shortDescription,
+//       reqBody.image,
+//     ],
+//     (err, result) => {
+//       // print error or respond with result.
+//       errorResult(err, result, response);
+//     }
+//   );
+// });
+// app.put("/artists/:id", async (request, response) => {
+//   const reqArtistId = request.params.id;
+//   const reqBody = request.body;
+//   connection.query(
+//     "UPDATE artists SET name = ?, birthdate = ? WHERE artistId = ?",
+//     [reqBody.name, reqBody.birthdate, reqArtistId],
+//     (err, result) => {
+//       // print error or respond with result.
+//       errorResult(err, result, response);
+//     }
+//   );
+// });
