0938db9070cce75d45eab43505471049bca93d7b
Maltemork <malte.sorensen@protonmail.com>
2023-11-29 14:04:43 +0100

    fix
    asdf

--- backend.js
+++ backend.js
@@ -1,5 +1,11 @@
 "use strict";
 
+//
+//  Backend Node.js app for musicoolio website.
+//  Talks to azure database, and does cool stuff!
+//
+
+// Import Express, FS and CORS.
 import express from "express";
 import cors from "cors";
 import connection from "./database.js";
@@ -9,8 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.MYSQL_PORT);
+app.listen(process.env.PORT);
 
+console.log("Hej! Det kører");
+
+app.get("/", (req, res) => res.json({ message: "Connection made!" }));
+
+// Default function for printing error or returning results.
 function errorResult(err, result, response) {
   if (err) {
     console.log(err);
@@ -18,6 +29,7 @@ function errorResult(err, result, response) {
     response.json(result);
   }
 }
+
 /* ------------ Sponsors ------------ */
 app.get("/sponsors", async (req, res) => {
   connection.query(
--- database.js
+++ database.js
@@ -1,18 +1,13 @@
 import mysql from "mysql2";
 import "dotenv/config";
-import fs from "fs";
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  port: process.env.MYSQL_PORT,
+  PORT: process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   multipleStatements: true,
 });
-
-if (process.env.MYSQL_CERT) {
-  connection.ssl = { cs: fs.readFileSync("periamma-server.crt.pem") };
-}
 
 export default connection;
